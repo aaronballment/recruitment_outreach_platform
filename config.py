c65eb931444a89ee3eccb880edccc6aa9288df2b
Aaron Ballment <199175436+aaronballment@users.noreply.github.com>
2026-06-24 16:59:10 +1000

    added env info to config

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SMARTLEAD_API_KEY = os.getenv("SMARTLEAD_API_KEY")
+LEAD_MAGIC_KEY = os.getenv("LEAD_MAGIC_KEY")
+AI_API_KEY = os.getenv("AI_API_KEY")
+APIFY_API_KEY = os.getenv("APIFY_API_KEY")
+
 MULTI_PART_TLDS = {
     'com.au', 'gov.au', 'net.au', 'edu.au', 'org.au'
 }
